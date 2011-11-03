56c12382bb13f858ef8623d6c3d83ebedf9eb128
Edward Hotchkiss <e@ingk.com>
2011-11-03 09:39:55 -0400

    request testing

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,10 @@
 var vows = require("vows");
 var assert = require("assert");
 var should = require("should");
+var request = require("request");
 
 // CORE
-var io = require("../")
+var io = require("../lib/io")
 
 // VOWS!
 vows.describe("General Module Tests").addBatch({
@@ -16,6 +17,14 @@ vows.describe("General Module Tests").addBatch({
     "io should be a function" : function(topic) {
       topic.should.be.a("function");
     },
+  },
+  "when making a request to google" : {
+    topic : function() {
+      request("http://www.google.com", this.callback);
+    },
+    "we should get back a 200" : function (error, response, body) {
+      assert.equal(response.statusCode, 200);
+    }
   }
 }).export(module);
 
